f2d70aaca8ca92fd4e22c8e136d732cf8f586f95
Wayand Bahramzy <lawangjan@hotmail.com>
2025-07-14 22:41:44 +0200

    refactor: fix python cache permission

--- boot.sh
+++ boot.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
 # this script is used to boot a Docker container
+# Clean Python cache files on startup
+echo "Cleaning Python cache files..."
+find /invoicer-api -name "__pycache__" -type d -exec rm -rf {} + 2>/dev/null || true
+find /invoicer-api -name "*.pyc" -delete 2>/dev/null || true
+echo "cache files cleaned"
 
-echo "I am Here.........starting while"
+echo "Starting application..."
 pip3 install -r requirements.txt
 
-## for i in $(pip list -o | awk 'NR > 2 {print $1}'); do pip install -U $i; done
-
-# while true; do
-#     flask db upgrade
-#     if [[ "$?" == "0" ]]; then
-#         break
-#     fi
-#     echo Deploy command failed, retrying in 5 secs...
-#     sleep 5
-# done
-
+flask db upgrade
 
 echo "I am Here.........DONE while"
-
 
 exec gunicorn -b :5000 --access-logfile - --error-logfile - run:app
