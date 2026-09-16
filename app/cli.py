251813ceb1a289ef7e7994b30fd65ce018c47324
Wayand Bahramzy <lawangjan@hotmail.com>
2026-09-16 22:38:43 +0200

    fix(seed): fix seeding and upgrade dependencies in requirements.txt

--- app/cli.py
+++ app/cli.py
@@ -62,9 +62,15 @@ def seed():
             if org_users:
                 for user_data in org_users:
                     try:
+<<<<<<< Updated upstream
                         user_data['organization_id'] = organization_obj.id
                         user_data['password_hash'] = User.generate_hash(user_data.get('password_hash'))
                         user_data['otp_secret'] = User.generate_otp_secret()
+=======
+                        user_data["organization_id"] = organization_obj.id
+                        user_data["otp_secret"] = User.generate_otp_secret()
+                        # print(user_data)
+>>>>>>> Stashed changes
                         user_obj = User(**user_data)
                         user_obj.save()
                         click.echo('-- created user: '+ user_data.get('name'))
